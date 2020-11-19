cb55467b6c00dc805ac839408469833b0f2f3ae8
FNG-2002 <fgundfb@gmail.com>
2020-11-19 10:38:52 +0100

    added tesseract for android (rmtheis). scanning working but not very accurate

--- BildScannerApp/app/src/main/java/com/fg/bildscannerapp/MainActivity.java
+++ BildScannerApp/app/src/main/java/com/fg/bildscannerapp/MainActivity.java
@@ -1,26 +1,45 @@
 package com.fg.bildscannerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
+import org.w3c.dom.Text;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static String TAG = "MainActivity";
 
+
     JavaCameraView javaCameraView;
     Mat mGrayCannyTest;
+    TesseractHelper tessHelper;
+
 
 
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(MainActivity.this) {
@@ -50,11 +69,46 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA},
+                1);
+
         javaCameraView = (JavaCameraView) findViewById(R.id.opencv_camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
         javaCameraView.setCvCameraViewListener(MainActivity.this);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        AssetManager assetManager = getAssets();
+
+        tessHelper = new TesseractHelper(assetManager, getWindow().getContext().getFilesDir());
+
+        TextView displayResult = (TextView) findViewById(R.id.displayResult);
+        Button scanButton = (Button) findViewById(R.id.button_scan);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayResult.setText("");
+                scanButton.setText("Bild wird analysiert...");
+                scanButton.setClickable(false);
+                new Thread(new Runnable() {
+                    public void run() {
+                        Bitmap b = Bitmap.createBitmap(mGrayCannyTest.width(), mGrayCannyTest.height(), Bitmap.Config.ARGB_8888);
+                        Utils.matToBitmap(mGrayCannyTest, b);
+                        String result = tessHelper.startOCR(b);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                scanButton.setText("Analysieren");
+                                scanButton.setClickable(true);
+                                displayResult.setText(result);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+
     }
 
     @Override
@@ -74,7 +128,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
         mGrayCannyTest = inputFrame.gray();
 
-        Imgproc.Canny(mGrayCannyTest, mGrayCannyTest, 60, 60*3);
+        //Imgproc.adaptiveThreshold(mGrayCannyTest, mGrayCannyTest, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 53, 34);
+        //Imgproc.Canny(mGrayCannyTest, mGrayCannyTest, 60, 60*3);
 
         return mGrayCannyTest;
     }
---
+++ BildScannerApp/app/src/main/java/com/fg/bildscannerapp/TesseractHelper.java
@@ -0,0 +1,87 @@
+package com.fg.bildscannerapp;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TesseractHelper extends AppCompatActivity {
+
+    public static final String TAG = "TESSERACTHELPER";
+
+    public static final String TESS_DATA = "/tessdata";
+
+    private TessBaseAPI tessBaseAPI;
+
+    //private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString()+"/Tess";
+    private static String DATA_PATH = "";
+
+
+    public TesseractHelper(AssetManager a, File path){
+        DATA_PATH = path.getAbsolutePath();
+        Log.d("LOL", DATA_PATH);
+        prepareTessData(a);
+    }
+
+    private void prepareTessData(AssetManager a){
+        try {
+            File dir = new File(DATA_PATH + TESS_DATA);
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+
+            String[] fileList = a.list("");
+            for (String fileName : fileList){
+                String pathToData = DATA_PATH+TESS_DATA+"/"+fileName;
+
+                if(!(new File(pathToData)).exists()){
+                    InputStream in = a.open(fileName);
+                    OutputStream out = new FileOutputStream(pathToData);
+
+                    byte[] buff = new byte[1024];
+                    int len;
+                    while((len = in.read(buff)) > 0){
+                        out.write(buff, 0, len);
+                    }
+                    in.close();
+                    out.close();
+                }
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public String startOCR(Bitmap image){
+        try {
+            tessBaseAPI = new TessBaseAPI();
+        } catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseAPI.init(DATA_PATH, "deu");
+        //tessBaseAPI.setVariable("tessedit_char_whitelist", " -@.abcdefghijklmnopqrstuvwxyzäöüÄÖÜABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+        tessBaseAPI.setImage(image);
+
+        String result = "Bisher kein Ergebnis";
+        try {
+            result = tessBaseAPI.getUTF8Text();
+        } catch (Exception e){
+            Log.e(TAG, e.getMessage());
+        }
+        tessBaseAPI.end();
+        return result;
+    }
+}
